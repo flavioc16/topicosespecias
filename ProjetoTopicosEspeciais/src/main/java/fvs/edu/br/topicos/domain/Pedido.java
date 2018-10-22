624c6f5ce151c4c219ae0891050902405efee5c3
Flavio sousa de Castro <flavio2008sousa@hotmail.com>
2018-10-22 20:54:37 -0200

    Relacionamento das classes dominio

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Pedido.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/domain/Pedido.java
@@ -7,7 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 @Entity
 public class Pedido implements Serializable  {
 	private static final long serialVersionUID = 1L;
@@ -16,68 +19,15 @@ public class Pedido implements Serializable  {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
 	private Date instante;
+	
 	@ManyToOne
+	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco endereco;
 	
-	public Pedido() {
-		
-	}
-
-	public Pedido(Integer id, Date instante, Endereco endereco) {
-		super();
-		this.id = id;
-		this.instante = instante;
-		this.endereco = endereco;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public Date getInstante() {
-		return instante;
-	}
-
-	public void setInstante(Date instante) {
-		this.instante = instante;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pedido other = (Pedido) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	public Endereco getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
+	@ManyToOne
+	@JoinColumn(name="cliente_id")
+	private Cliente cliente;
 	
 }
